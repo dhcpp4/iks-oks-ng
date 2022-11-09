944846b21cb6c2fa39b77d6d33696df7d7aac2f3
default <default@default.lt>
2022-11-09 13:33:50 +0100

    rewrite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'iks-oks-ng';
+
+  title: string = "iks-oks-ng";
+
+  player: 1 | 2 = 1;
+
+  situation: Array<0 | 1> = [
+    0, 0, 0,
+    0, 0, 0,
+    0, 0, 0
+  ];
+
+  message : string = "";
+
+  currentClasses = {
+    clicked: false
+  };
+
+  onClick(e: Event) : void {
+    let square = e.target as HTMLElement;
+    this.currentClasses.clicked = true; //square.style.pointerEvents = "none";
+    this.situation[Number(square.id)] = 1;
+    this.isGameOver();
+  }
+
+  isGameOver() : void {
+    if (this.situation[0] && this.situation[1] && this.situation[2] ||
+        this.situation[3] && this.situation[4] && this.situation[5] ||
+        this.situation[6] && this.situation[7] && this.situation[8] ||
+        this.situation[0] && this.situation[3] && this.situation[6] ||
+        this.situation[1] && this.situation[4] && this.situation[7] ||
+        this.situation[2] && this.situation[5] && this.situation[8] ||
+        this.situation[0] && this.situation[4] && this.situation[8] ||
+        this.situation[2] && this.situation[4] && this.situation[6]) {
+      this.message = "Game Over";
+    }
+  }
+
+
 }
