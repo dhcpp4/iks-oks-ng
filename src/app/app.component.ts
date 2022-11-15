1843b84966881dd98316560a591490b145ac5d10
default <default@default.lt>
2022-11-15 20:54:45 +0100

    refines isGameOver()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,28 +32,14 @@ export class AppComponent {
       [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
     ];
 
-    return victoryCombinations.find(el =>
-      this.situation[el[0]] == 'X' && this.situation[el[1]] == 'X' && this.situation[el[2]] == 'X' ||
-      this.situation[el[0]] == 'O' && this.situation[el[1]] == 'O' && this.situation[el[2]] == 'O'
-    ) != undefined;
-
-    // let check = (symbol: string) => {
-    //   return this.situation[0] == symbol && this.situation[1] == symbol && this.situation[2] == symbol
-    // }
-
-    // [0, 1, 2].reduce((previous: number, current: number) =>
-    //   this.situation[previous] == 'X' && this.situation[current] == 'X')
-
-
-    // let xInd: number[] = [], yInd: number[] = [];
-    // this.situation.forEach((el, ind) => {
-    //   if (el == 'X') xInd.push(ind);
-    //   else if (el == 'O') yInd.push(ind);
-    // })
-    // console.log(xInd);
-    // return victoryCombinations.every((combination) =>
-    //   xInd.indexOf(combination) > -1 || yInd.indexOf(combination) > -1);
+    return victoryCombinations.some(
+      el => {
+        let temp = this.indexByArray(this.situation, el);
+        return temp.every(e => e == 'X') || temp.every(e => e == 'O')
+      })
   }
 
-
+  private indexByArray(arr: any[], ind: number[]) : any[] {
+    return ind.map(e => arr[e]);
+  }
 }
